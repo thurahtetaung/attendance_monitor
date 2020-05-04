291708332c7721609fa10f4cba504368636a2a78
Thura Htet Aung <thurahtetaung711@gmail.com>
2020-05-04 16:50:26 +0630

    Change layout

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("layout.html")
 
 @app.route('/upload',methods=['POST'])
 def hello():
