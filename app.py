a9ed1a6429bbe4958f0612582c802555885a95b2
Thura Htet Aung <thurahtetaung711@gmail.com>
2021-01-26 14:00:19 +0630

    Implemented API for upload and recognize

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from matplotlib.pyplot import imshow
 import numpy as np
 import face_recognition
 from itertools import compress
-<<<<<<< HEAD
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
@@ -96,37 +95,6 @@ def load_image_file(file, mode='RGB'):
     img = img.convert(mode)
 
     return np.array(img)
-=======
-
-app = Flask(__name__)
-
-obama_image = face_recognition.load_image_file("faces/obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-thura_image = face_recognition.load_image_file("faces/thura.jpg")
-thura_face_encoding = face_recognition.face_encodings(thura_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("faces/biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-mom_image = face_recognition.load_image_file("faces/mom.jpg")
-mom_face_encoding = face_recognition.face_encodings(mom_image)[0]
-
-known_face_encodings = [
-    obama_face_encoding,
-    thura_face_encoding,
-    biden_face_encoding,
-    mom_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Thura Htet Aung",
-    "Joe Biden",
-    "Daw Thida Oo"
-]
->>>>>>> 6cb1564ee7548c3f1bac7a114c9accc274fd5d84
 
 @app.route("/")
 def index():
@@ -134,7 +102,6 @@ def index():
 
 @app.route('/upload',methods=['POST'])
 def hello():
-<<<<<<< HEAD
     print(request)
     data = request.files['image']
     name = request.values['userid']
@@ -164,19 +131,6 @@ def hello():
         print(result)
         return {'Present': bool(result[0])}
     return {'Present': False}
-=======
-    data_url = request.values['imageBase64']
-    data_url= data_url[22:]
-    name = request.values['username']
-    im = Image.open(BytesIO(base64.b64decode(data_url)))
-    im.save("images/" + name + '.jpg')
-    fr = face_recognition.load_image_file("images/" + name + '.jpg')
-    unknown_face_encoding = face_recognition.face_encodings(fr)
-    result = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding[0])
-    face_name = list(compress(known_face_names, result))
-    print(face_name[0])
-    return ''
->>>>>>> 6cb1564ee7548c3f1bac7a114c9accc274fd5d84
 
 if(__name__ == '__main__'):
     app.run(host='0.0.0.0',port=5000, ssl_context = ('cert.pem', 'key.pem'))
